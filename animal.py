b0219f3a63820b8787e41c23f3d7a7fd99643f24
rodrigosayal <rodrigosayal@gmail.com>
2025-09-17 19:45:19 -0300

    Update animal.py

--- animal.py
+++ animal.py
@@ -1,63 +1,16 @@
 from abc import ABC, abstractclassmethod
 
 class Animal(ABC):
-    def __init__(self, name, velocidad):
+    def __init__(self, name):
         self.name = name
         self.posicion = 0
-        self.velocidad = velocidad
     
-    
-    def caminar (self, tiempo):
-        self.posicion = self.velocidad * tiempo
+    @abstractclassmethod
+    def caminar (self):
+        self.posicion
 
     
 
-class Perro(Animal):
-    def __init__(self, name, ):
-        super().__init__(name, 2)
-
-class Gato(Animal):
-    def __init__(self, name,):
-        super().__init__(name, 1)
-
-
-c = Perro("Rodro")
-g = Gato("Pedro")
-
-c.caminar(10)
-g.caminar(10)
-
-print(c.posicion)
-print(g.posicion)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-Eliminar tiempo y velocidad del ANIMAL
 class Perro1(Animal):
 
     def __init__(self, name, velocidad):
@@ -79,4 +32,3 @@ p = Perro1 ("Cesar",2)
 u = Perro2 ("Feca",1)
 print(p.velocidad)
 print(u.name)
-"""
