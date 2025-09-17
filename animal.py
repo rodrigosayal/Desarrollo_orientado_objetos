6a17d0da0bedc009c49eb2509be0e0effb30f3a0
rodrigosayal <rodrigosayal@gmail.com>
2025-09-17 19:46:17 -0300

    Update animal.py

--- animal.py
+++ animal.py
@@ -1,34 +1,31 @@
 from abc import ABC, abstractclassmethod
 
 class Animal(ABC):
-    def __init__(self, name):
+    def __init__(self, name, velocidad):
         self.name = name
         self.posicion = 0
-    
-    @abstractclassmethod
-    def caminar (self):
-        self.posicion
-
-    
-
-class Perro1(Animal):
-
-    def __init__(self, name, velocidad):
-        super().__init__(name)
         self.velocidad = velocidad
-        
-    def caminar(self): 
-        self.posicion = self.posicion + self.velocidad   
+    
+    
+    def caminar (self, tiempo):
+        self.posicion = self.velocidad * tiempo
 
-class Perro2(Animal):
+    
 
-    def hablar(self):
-        print("hola")
-        
-    def caminar(self): 
-        self.posicion = self.posicion + self.velocidad   
+class Perro(Animal):
+    def __init__(self, name, ):
+        super().__init__(name, 2)
 
-p = Perro1 ("Cesar",2)
-u = Perro2 ("Feca",1)
-print(p.velocidad)
-print(u.name)
+class Gato(Animal):
+    def __init__(self, name,):
+        super().__init__(name, 1)
+
+
+c = Perro("Rodro")
+g = Gato("Pedro")
+
+c.caminar(10)
+g.caminar(10)
+
+print(c.posicion)
+print(g.posicion)
